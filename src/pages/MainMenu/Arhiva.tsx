1085291f65f387145c0452a287513b0b61f95dfe
martytrajkoski <martytrajkoski@gmail.com>
2025-06-04 14:58:40 +0200

    archive search DONE, profile-modal styling fixed

--- src/pages/MainMenu/Arhiva.tsx
+++ src/pages/MainMenu/Arhiva.tsx
@@ -3,14 +3,36 @@ import axiosClient from "../../axiosClient/axiosClient";
 import type { FakturaType } from "../../types/types";
 import InvoiceCard from "../../components/MainMenu/InvoiceCard";
 import Pagination from "../../components/Pagination/Pagination";
+import { useNavigate } from "react-router-dom";
+
+
+const getRouteByRole = (role: string, br_faktura: number): string => {
+  switch (role) {
+    case "Продекан за финансии":
+      return `/prodekan/${br_faktura}`;
+    case "Технички секретар":
+      return `/tehnickisekretar/${br_faktura}`;
+    case "Јавна набавка":
+      return `/tipnabavka/${br_faktura}`;
+    case "Барател на набавка":
+      return `/baratelnabavka/${br_faktura}`;
+    case "Сметководство":
+      return `/smetkovodstvo/${br_faktura}`;
+    default:
+      return "/";
+  }
+};
 
 const Arhiva: React.FC = () => {
+  const navigate = useNavigate();
   const [archiveFaktura, setArchiveFaktura] = useState<FakturaType[]>([]);
   const [fakturaLastPage, setFakturaLastPage] = useState<number>();
   const [fakturaCurrentPage, setFakturaCurrentPage] = useState<number>(1);
+  const [filteredFaktura, setFilteredFaktura] = useState<FakturaType[]>([]);
+  const [search, setSearch] = useState<string>("")
   const [role, setRole] = useState<string>("");
 
-  const fetchFakturas = async () => {
+  const fetchArhivedFakturas = async () => {
     try {
       const response = await axiosClient.get(`/faktura/archive/?page=${fakturaCurrentPage}`);
 
@@ -24,6 +46,23 @@ const Arhiva: React.FC = () => {
     }
   };
 
+  const handleSearchChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.trim();
+    setSearch(value);
+
+    try {
+      const response = await axiosClient.post('/faktura/searcharchive',{
+        search: value
+      })
+
+      if(response.status === 201){
+        setFilteredFaktura(response.data.documents)
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   const fetchUser = async () => {
     try {
       const response = await axiosClient.get("/auth/user");
@@ -36,12 +75,38 @@ const Arhiva: React.FC = () => {
   };
 
   useEffect(() => {
-    fetchFakturas();
+    fetchArhivedFakturas();
     fetchUser();
   }, []);
 
   return (
     <div className="mainmenu-content">
+        <div className="mainmenu-search">
+        <input
+          type="search"
+          placeholder="Пребарај фактура..."
+          value={search}
+          onChange={handleSearchChange}
+        />
+        {search.length > 0 && (
+          <ul className="dropdown">
+            {filteredFaktura.map((item) => {
+              return (
+                <li
+                  onClick={() =>
+                    navigate(getRouteByRole(role, item.br_faktura))
+                  }
+                  key={item.id}
+                  className="dropdown-item"
+                >
+                  <span>Број на фактура: {item.br_faktura}</span>
+                </li>
+              );
+            })}
+            {filteredFaktura.length === 0 && <li>Нема резултати</li>}
+          </ul>
+        )}
+      </div>
       <div className="mainmenu-invoices">
         <h1>Архива</h1>
         <InvoiceCard items={archiveFaktura} role={role} />
--- src/pages/Sign/ProfileModal.tsx
+++ src/pages/Sign/ProfileModal.tsx
@@ -1,18 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axiosClient from "../../axiosClient/axiosClient";
 import PasswordChangeModal from "./PasswordChangeModal";
 import type { PropsModal } from "../../types/types";
+import { faClose } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ProfileModal: React.FC<PropsModal> = ({ onClose }) => {
   const [user, setUser] = useState({ name: "", email: "" });
   const [showPasswordModal, setShowPasswordModal] = useState(false);
   const [message, setMessage] = useState<string | null>(null);
+  const modalRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     axiosClient.get("/auth/user").then((res) => {
       setUser(res.data);
     });
   }, []);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+        onClose?.();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [onClose]);
 
   const handleUpdate = async () => {
     setMessage(null);
@@ -26,25 +42,27 @@ const ProfileModal: React.FC<PropsModal> = ({ onClose }) => {
 
   return (
     <div className="modal-overlay">
-      <div className="modal-content">
+      <div className="modal-content" ref={modalRef}>
         <div className="title">
           <h2>Мој Профил</h2>
-          <button className="close-button" onClick={() => onClose?.()}>
-            x
-          </button>
+          <FontAwesomeIcon icon={faClose} className="close-button" onClick={() => onClose?.()}/>
         </div>
-        <label>Име:</label>
-        <input
-          type="text"
-          value={user.name}
-          onChange={(e) => setUser({ ...user, name: e.target.value })}
-        />
-        <label>E-mail:</label>
-        <input
-          type="email"
-          value={user.email}
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
-        />
+        <div className="label-input">
+          <label>Име:</label>
+          <input
+            type="text"
+            value={user.name}
+            onChange={(e) => setUser({ ...user, name: e.target.value })}
+          />
+        </div>
+        <div className="label-input">
+          <label>E-mail:</label>
+          <input
+            type="email"
+            value={user.email}
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          />
+        </div>
         <div className="update-button">
           <button onClick={handleUpdate}>Зачувај</button>
           <button onClick={() => setShowPasswordModal(true)}>
