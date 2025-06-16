f261694ecccde30347f2cb6968851c5c52968da9
Bojan Jovanoski <jovanoskibojan.w@gmail.com>
2025-06-16 12:43:57 +0200

    spoeno tip nabavka so baratel

--- src/pages/Departments/Prodekan/Prodekan.tsx
+++ src/pages/Departments/Prodekan/Prodekan.tsx
@@ -10,7 +10,8 @@ const Prodekan: React.FC = () => {
   const navigate = useNavigate();
 
   const [showConfirmModal, setShowConfirmModal] = useState<boolean>(false);
-  const [showUnauthorizedModal, setShowUnauthorizedModal] =useState<boolean>(false);
+  const [showUnauthorizedModal, setShowUnauthorizedModal] =
+    useState<boolean>(false);
 
   // TEHNICKI SEKRETAR
   const [arhivski_br, setArhivski_br] = useState<string>("");
@@ -58,12 +59,10 @@ const Prodekan: React.FC = () => {
   // COMMENTS
   const [commentTehnicki, setCommentTehnicki] = useState<string>("");
   const [commentTipNabavka, setCommentTipNabavka] = useState<string>("");
-  const [commentBaratel, setCommentBaratel] = useState<string>("");
   const [commentSmetkovodstvo, setCommentSmetkovodstvo] = useState<string>("");
 
   const [statusTehnicki, setStatusTehnicki] = useState<string>("");
   const [statusTipNabavka, setStatusTipNabavka] = useState<string>("");
-  const [statusBaratel, setStatusBaratel] = useState<string>("");
   const [statusSmetkovodstvo, setStatusSmetkovodstvo] = useState<string>("");
 
   const fetchFakturas = async () => {
@@ -85,7 +84,6 @@ const Prodekan: React.FC = () => {
         );
         setCommentTehnicki(data.tehnicki_sekretar?.review_comment ?? "");
         setStatusTehnicki(data.tehnicki_sekretar?.status ?? "");
-
 
         // Tip Nabavka
         setTip(data.tip_nabavka?.tip ?? "javna");
@@ -112,7 +110,6 @@ const Prodekan: React.FC = () => {
         setCommentTipNabavka(data.tip_nabavka?.review_comment ?? "");
         setStatusTipNabavka(data.tip_nabavka?.status ?? "");
 
-
         // Baratel Nabavka
         setBaratel(data.baratel_javna_nabavka?.baratel.name ?? undefined);
         setBrKartonBaratel(data.baratel_javna_nabavka?.br_karton ?? undefined);
@@ -123,9 +120,6 @@ const Prodekan: React.FC = () => {
           data.baratel_javna_nabavka.updated_by?.name ??
             data.baratel_javna_nabavka.submited_by?.name
         );
-        setCommentBaratel(data.baratel_javna_nabavka?.review_comment ?? "");
-        setStatusBaratel(data.baratel_javna_nabavka?.status ?? "");
-
 
         // Smetkovodstvo
         setBrKartonSmetkovodstvo(data.smetkovodstvo?.br_karton ?? undefined);
@@ -144,7 +138,6 @@ const Prodekan: React.FC = () => {
         );
         setCommentSmetkovodstvo(data.smetkovodstvo?.review_comment ?? "");
         setStatusSmetkovodstvo(data.smetkovodstvo?.status ?? "");
-
 
         //Prodekan
         setDatumProdekan(
@@ -165,7 +158,6 @@ const Prodekan: React.FC = () => {
     const hasRejectedSection =
       statusTehnicki === "rejected" ||
       statusTipNabavka === "rejected" ||
-      statusBaratel === "rejected" ||
       statusSmetkovodstvo === "rejected";
 
     if (hasRejectedSection) {
@@ -192,7 +184,7 @@ const Prodekan: React.FC = () => {
 
       if (response.status === 201) {
         console.log("Фактурата е успешно запечатена.");
-        navigate('/');
+        navigate("/");
       }
 
       if (response.status === 401) {
@@ -304,15 +296,6 @@ const Prodekan: React.FC = () => {
         </div>
       )}
 
-      {!isSealed && (
-        <CommentSection
-          brFaktura={br_faktura ?? ""}
-          endpoint="/prodekan/updatedepartmentstatus/tip"
-          initialStatus={statusTipNabavka}
-          initialComment={commentTipNabavka}
-        />
-      )}
-
       <div className="form-item">
         <h3>
           3. Информации за евиденција (одделение за јавна набавка или барател на
@@ -339,9 +322,9 @@ const Prodekan: React.FC = () => {
       {!isSealed && (
         <CommentSection
           brFaktura={br_faktura ?? ""}
-          endpoint="/prodekan/updatedepartmentstatus/baratel"
-          initialStatus={statusBaratel}
-          initialComment={commentBaratel}
+          endpoint="/prodekan/updatedepartmentstatus/tip"
+          initialStatus={statusTipNabavka}
+          initialComment={commentTipNabavka}
         />
       )}
 
@@ -352,7 +335,10 @@ const Prodekan: React.FC = () => {
           <input type="text" value={brKartonSmetkovodstvo ?? ""} readOnly />
           <label>Состојба на картон:</label>
           <input type="text" value={sostojbaKarton} readOnly />
-          <label>Предметот на набавка има основа за евидентирање како основно средство(а):</label>
+          <label>
+            Предметот на набавка има основа за евидентирање како основно
+            средство(а):
+          </label>
           <input
             type="text"
             value={osnovaEvidentiranje ? "Да" : "Не"}
@@ -361,7 +347,8 @@ const Prodekan: React.FC = () => {
           <label>Пополнет е формулар за задолжување на основно средство:</label>
           <input type="text" value={formular ? "Да" : "Не"} readOnly />
           <label>
-            Средствата се внесени (поединечно) како новонабавени за тековната година:
+            Средствата се внесени (поединечно) како новонабавени за тековната
+            година:
           </label>
           <input type="text" value={vneseniSredstva ? "Да" : "Не"} readOnly />
           <label>Предлог сметка за наплата од:</label>
@@ -410,7 +397,9 @@ const Prodekan: React.FC = () => {
               Одобри ја фактурата
             </button>
           ) : (
-            <button onClick={()=>navigate(`/pdf/${br_faktura}`)}>Печати</button>
+            <button onClick={() => navigate(`/pdf/${br_faktura}`)}>
+              Печати
+            </button>
           )}
         </div>
       </div>
--- src/pages/Departments/TipNabavka/TipNabavka.tsx
+++ src/pages/Departments/TipNabavka/TipNabavka.tsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axiosClient from "../../../axiosClient/axiosClient";
 import { useParams } from "react-router-dom";
-import BaratelNabavka from "../BaratelNabavka/BaratelNabavka";
 import CommentSectionRead from "../../../components/Comment-Section/Comment-Section-Read";
 import SweetAlert from "../../../components/Sweet-Alert/Sweet-Alert";
+import type { Baratel } from "../../../types/types";
+
 
 const TipNabavka: React.FC = () => {
   const { br_faktura } = useParams<string>();
@@ -11,6 +12,7 @@ const TipNabavka: React.FC = () => {
   const [showDeleteModal, setShowDeleteModal] = useState<boolean>(false);
   const [showUpdateModal, setShowUpdateModal] = useState<boolean>(false);
 
+  //TIP
   const [tip, setTip] = useState<string>("javna");
   const [datum, setDatum] = useState<string>();
   const [review_comment, setReview_comment] = useState<string>();
@@ -27,9 +29,19 @@ const TipNabavka: React.FC = () => {
   const [created, setCreated] = useState<boolean>();
   const [documentId, setDocumentId] = useState<number>();
 
+  //EVIDENCIJA
+
+  const [brKarton, setBrKarton] = useState<number>();
+  const [nazivProekt, setNazivProekt] = useState<string>("");
+  const [poteklo, setPoteklo] = useState<string>("");
+  const [baratelId, setBaratelId] = useState<number>();
+  const [barateli, setBarateli] = useState<Baratel[]>([]);
+
   useEffect(() => {
     showTipNabavka();
-  }, [status]);
+    fetchBarateli();
+    showBaratel();
+  }, []);
 
   const showTipNabavka = async () => {
     try {
@@ -139,11 +151,11 @@ const TipNabavka: React.FC = () => {
 
   const deleteTipNabavka = async () => {
     try {
-      const response = await axiosClient.delete(
+      const responseTip = await axiosClient.delete(
         `/tipnabavka/destroy/${documentId}`
       );
 
-      if (response.status === 201) {
+      if (responseTip.status === 201) {
         console.log("Tip nabavka is deleted");
 
         setCreated(false);
@@ -159,18 +171,133 @@ const TipNabavka: React.FC = () => {
 
         setShowDeleteModal(false);
       }
+
+       const responseBaratel = await axiosClient.delete(
+        `/baratelnabavka/destroy/${documentId}`
+      );
+
+      if (responseBaratel.status === 201) {
+        setDocumentId(undefined);
+        setCreated(false);
+        setBaratelId(undefined);
+        setBrKarton(0);
+        setNazivProekt("");
+        setPoteklo("");
+        setDatum("");
+
+        setShowDeleteModal(false);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+
+    try {
+     
     } catch (error) {
       console.error(error);
     }
   };
+
+  const fetchBarateli = async () => {
+    try {
+      const response = await axiosClient.get("/barateli/");
+      setBarateli(response.data);
+    } catch (error) {
+      console.error("Failed to fetch barateli", error);
+    }
+  };
+  
+  const showBaratel = async () => {
+    try {
+      const response = await axiosClient.get(
+        `/baratelnabavka/show/${br_faktura}`
+      );
+      
+      if (response.status === 201) {
+        setIs_sealed(response.data.is_sealed);
+        setDocumentId(response.data.document.id);
+        setBrKarton(response.data.document.br_karton);
+        setNazivProekt(response.data.document.naziv_proekt);
+        setPoteklo(response.data.document.poteklo);
+        setDatum(response.data.document.datum);
+        setBaratelId(response.data.document.baratel_id);
+        setReview_comment(response.data.document.review_comment);
+        setStatus(response.data.document.status);
+        setCreated(true);
+      } else if (response.status === 404) {
+        setBrKarton(undefined);
+        setNazivProekt("");
+        setPoteklo("");
+        setDatum("");
+        setBaratelId(undefined);
+        setCreated(false);
+        setReview_comment("");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const storeBaratelNabavka = async (e: any) => {
+    e.preventDefault();
+    
+    try {
+      const response = await axiosClient.post("/baratelnabavka/addDocument", {
+        br_faktura: br_faktura || "0",
+        br_karton: brKarton,
+        naziv_proekt: nazivProekt,
+        poteklo,
+        datum,
+        baratel_id: baratelId,
+        read: true,
+      });
+
+      if (response.status === 201) {
+        console.log("BaratelNabavka stored");
+        setDocumentId(response.data.document.id);
+        setCreated(true);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const updateBaratelNabavka = async (e: any) => {
+    e.preventDefault();
+
+    try {
+      const response = await axiosClient.patch(
+        `/baratelnabavka/updateDocument/${documentId}`,
+        {
+          br_faktura: br_faktura || "0",
+          br_karton: brKarton,
+          naziv_proekt: nazivProekt,
+          poteklo,
+          datum,
+          baratel_id: baratelId,
+        }
+      );
+
+      if (response.status == 201) {
+        setStatus("pending");
+        setShowUpdateModal(true);
+        console.log("BaratelNabavka is updated successfully");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
 
   return (
     <>
       <form onSubmit={(e) => {e.preventDefault();
         if (!created) {
           storeTipNabavka(e);
+          storeBaratelNabavka(e);
         } else {
           updateTipNabavka(e);
+          updateBaratelNabavka(e);
         }
       }}>
         <div className="form-item">
@@ -311,39 +438,126 @@ const TipNabavka: React.FC = () => {
           </div>
         )}
 
-        <div className="form-buttons">
-          <div></div>
-          <div className="form-buttons-edit">
-            {is_sealed === 0 && (
-              <>
-                {!created ? (
-                  <button type="submit">Зачувај</button>
-                ) : (
-                  <>
-                    <button onClick={(e) => updateTipNabavka(e)}>Измени</button>
-                    <button type="button" onClick={() => {setShowDeleteModal(true)}}>Избриши</button>
-                  </>
-                )}
-              </>
-            )}
-          </div>
-        </div>
+        <div className="form-item">
+                  <h1>Информации за евиденција</h1>
+        
+                  <div className="form-item-inputs">
+                    <label>Избери барател</label>
+                    <select
+                      value={baratelId}
+                      disabled={Boolean(is_sealed)}
+                      onChange={(e) => setBaratelId(Number(e.target.value))}
+                      required
+                    >
+                      <option value="">-- Избери барател --</option>
+                      {barateli.map((b) => (
+                        <option key={b.id} value={b.id}>
+                          {b.name}
+                        </option>
+                      ))}
+                    </select>
+        
+                    <label>Број на картон (Конто)</label>
+                    <input
+                      type="number"
+                      value={brKarton}
+                      readOnly={Boolean(is_sealed)}
+                      onChange={(e) => setBrKarton(Number(e.target.value))}
+                      required
+                    />
+        
+                    <label>Назив на проектот</label>
+                    <input
+                      type="text"
+                      value={nazivProekt}
+                      readOnly={Boolean(is_sealed)}
+                      onChange={(e) => setNazivProekt(e.target.value)}
+                      required
+                    />
+                  </div>
+        
+                  <div className="form-item-radio">
+                    <label>Потекло на финансиите:</label>
+                    <div className="form-radio">
+                      <label>
+                        <input
+                          type="radio"
+                          name="poteklo"
+                          value="Средства на МФС"
+                          checked={poteklo === "Средства на МФС"}
+                          disabled={Boolean(is_sealed)}
+                          onChange={(e) => setPoteklo(e.target.value)}
+                          required
+                        />&nbsp;
+                        Средства на МФС
+                      </label>
+                      <label>
+                        <input
+                          type="radio"
+                          name="poteklo"
+                          value="Буџет"
+                          checked={poteklo === "Буџет"}
+                          disabled={Boolean(is_sealed)}
+                          onChange={(e) => setPoteklo(e.target.value)}
+                          required
+                        />&nbsp;
+                        Буџет
+                      </label>
+                    </div>
+                  </div>
+        
+                  <div className="form-item-inputs">
+                    <label>Датум</label>
+                    <input
+                      type="date"
+                      value={datum}
+                      readOnly={Boolean(is_sealed)}
+                      onChange={(e) => setDatum(e.target.value)}
+                      required
+                    />
+                  </div>
 
-        {status !== "pending" ? (
-          <CommentSectionRead
-            review_comment={review_comment || ""}
-            status={status || "pending"}
-          />
-        ) : (
-          <div></div>
-        )}
+        
+                  <div className="form-buttons">
+                    <div></div>
+                    <div className="form-buttons">
+                      <div></div>
+                      <div className="form-buttons-edit">
+                        {is_sealed === 0 && (
+                          <>
+                            {!created ? (
+                              <button type="submit">Зачувај</button>
+                            ) : (
+                              <>
+                                <button onClick={() => {
+                                  updateBaratelNabavka({}); 
+                                  updateTipNabavka({});
+                                  }}>Измени</button>
+                                <button onClick={() => setShowDeleteModal(true)}>Избриши</button>
+                              </>
+                            )}
+                          </>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                  {status !== "pending" ? (
+                    <CommentSectionRead
+                      review_comment={review_comment || ""}
+                      status={status || "pending"}
+
+                    />
+                  ) : (
+                    <div></div>
+                  )}
+                </div>
       </form>
       <SweetAlert
         visibility={showDeleteModal}
         onConfirm={deleteTipNabavka}
         onCancel={() => setShowDeleteModal(false)}
         confirmButton="Избриши"
-        message="Дали сте сигурни дека сакате да ја избришите оваа секција?"
+        message="Дали сте сигурни дека сакате да ја избришeте оваа секција?"
       />
       <SweetAlert
         visibility={showUpdateModal}
@@ -352,7 +566,8 @@ const TipNabavka: React.FC = () => {
         confirmButton=""
         message="Промените се успешно реализирани"
       />
-      <BaratelNabavka />
+
+
       
     </>
   );
