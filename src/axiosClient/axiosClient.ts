f4493b25c8f4e14f9562689a4d157c50376c2fcb
Bojan Jovanoski <jovanoskibojan.w@gmail.com>
2025-06-20 11:52:46 +0200

    added axios ip

--- src/axiosClient/axiosClient.ts
+++ src/axiosClient/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'http://10.10.0.6:8000/api',
   headers: {
     'Accept': 'application/json',
   },
