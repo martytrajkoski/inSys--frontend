b6e59726fa83b8a1d6d33e63a1d609027d0d8acc
Bojan Jovanoski <jovanoskibojan.w@gmail.com>
2025-10-24 11:35:20 +0200

    adapted api url

--- src/axiosClient/axiosClient.ts
+++ src/axiosClient/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'http://insys-be.com/api',
   headers: {  
     'Accept': 'application/json',
   },
